a8efd51d097617083a1be01788841d20b9eec4a5
lmk206 <59675250+lmk206@users.noreply.github.com>
2020-03-26 17:36:39 +0900

    portfolio_eveonline
    html, css, script, jquery

---
+++ portfolio_eveonline/js/index.js
@@ -0,0 +1,84 @@
+
+
+window.addEventListener('DOMContentLoaded',function(){
+    //start
+    var header = document.querySelector('.header_wrap');
+    var nav = document.querySelector('nav');
+    var li = document.querySelectorAll('nav ul li');
+    var twoDepth = document.querySelectorAll('nav ul div');
+    var i = 0;
+
+    for(let i = 0; i<li.length;i++){
+        li[i].addEventListener('mouseover',function(e){
+            twoDepth[i].style.display = "block"
+        });
+        li[i].addEventListener('mouseleave',function(e){
+            twoDepth[i].style.display = "none"
+        });
+    }
+    //end
+
+    $(function(){
+        //start
+        // $('.visual figure > img').not(":first").hide();
+        var idx = 0;
+        var interval = 0;
+        function loop(){
+            interval = setInterval(function(){
+                $('.visual figure > img').eq(idx).fadeOut(1000);
+                    idx++;
+                    if(idx == 4){
+                        idx = 0
+                    }
+                $('.visual figure > img').eq(idx).fadeIn(1000);
+            },6000);
+        }; 
+        loop();
+        // function loop(){
+        //     interval = setInterval(function(){
+        //         $('.visual img').eq(idx).removeClass('active');
+        //             idx++;
+        //             if(idx == 4){
+        //                 idx = 0
+        //             }
+        //         $('.visual img').eq(idx).show().addClass("active");
+        //     },4000);
+        // };  
+        // loop();
+        //end
+            console.log(loop)
+        // start
+            var indi = document.querySelector('.indigator');
+            var indiLi = document.querySelectorAll('.indigator li');
+            var visualImg = document.querySelectorAll('.visual img');
+            var j=0;
+            for(let i = 0; i<indiLi.length;i++){
+                indiLi[i].addEventListener('mouseover',function(e){
+                    indiLi[i].classList.add('active');
+                    // console.log(indiLi[i])
+                    clearInterval(interval)
+                });
+                indiLi[i].addEventListener('mouseleave',function(e){
+                    indiLi[i].classList.remove('active');
+                    // console.log(indiLi[i])
+                    loop();
+                });
+        }
+    
+        for(let i = 0; i<indiLi.length;i++){
+            // visualImg[0].classList.add('active');
+
+            indiLi[i].addEventListener('click',function(e){
+
+                console.log(visualImg[i])
+                for(let j = 0; j<indiLi.length;j++){
+                visualImg[j].classList.remove('active')
+                }
+                visualImg[i].classList.add('active');
+                j=i
+            });
+        }
+    });
+    // end
+    
+});
