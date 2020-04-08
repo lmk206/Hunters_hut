b121ed28d3e843947291ae0052aad38ae5cb48c5
lmk206 <59675250+lmk206@users.noreply.github.com>
2020-04-08 18:26:04 +0900

    200408
    study

--- portfolio_eveonline/js/index.js
+++ portfolio_eveonline/js/index.js
@@ -1,45 +1,64 @@
 window.addEventListener('DOMContentLoaded',function(){
 //     //start
+// media query 이벤트 입니다
+var mql = window.matchMedia("screen and (max-width: 1024px)");
+var mq2 = window.matchMedia("screen and (max-width: 768px)");
+var mq3 = window.matchMedia("screen and (max-width: 480px)");
+var mq4 = window.matchMedia("screen and (max-width: 320px)");
 
-//window scroll event 입니다.
-$(function(){
-    var move = 0, num = 0, bln = true;
-    $('html').scrollTop();
-    $('section > div').on('mousewheel',function(e){
+mql.addListener(function(e) {
+    if(e.matches) {
+        console.log('모바일 화면 입니다.');
         
-        var wheel = e.originalEvent.wheelDelta;
-        if(wheel < 0){
-            if(num == 5){ return }
-            if($(this).next().index() != -1){
-                move = $(this).next().offset().top;    
-            }
-        }else{
-            if(num == 0){ return }
-            if(num > 0){
-                if($(this).next().index() != 4)
-                move = $(this).prev().offset().top;
-            }
-        }
-        $('html').stop().animate({
-            scrollTop : move
-        },1000,function(){
-            if(wheel < 0){
-                num++;
-            }else{
-                num--;
-            }
-        })
-    })
+    } else {
+        console.log('데스크탑 화면 입니다.');
+        action();
+        facAction();
+        guideAction();
+        
+    }
 });
+    //window scroll event 입니다.
+
+    function action(){
+        var move = 0, num = 0, bln = true;
+        $('html').scrollTop(0);
+        $(window).on('mousewheel',function(e){
+            var wheel = e.originalEvent.wheelDelta;
+            console.log(wheel)
+            if(bln){
+                bln = false;
+                if(wheel < 0){
+                    num++;
+                }else{
+                    num--;
+                }
+                $('.wSlide').each(function(i){
+                    if(num == i){
+                        
+                        move = $(this).offset().top;
+                        console.log(move)
+                    }   
+                })
+                
+                // move = num * $(window).height();
+                $('html').stop().animate({
+                    scrollTop : move
+                },700,function(){
+                    bln = true;
+                })
+            }  
+        })
+    }
 
     // 네비 떨구는 스크립트 입니다.
     $(function(){
-        $('nav li').on('mouseover',function(){
-            // $(this).find('div').addClass('show');
+        $('nav ul').on('mouseover',function(){
+            $(this).find('div').addClass('show');
             $('nav li').find("div").stop().slideDown()
         })
-        $('nav li').on('mouseleave',function(){
-            // $(this).find('div').removeClass('show');
+        $('nav ul').on('mouseleave',function(){
+            $(this).find('div').removeClass('show');
             $('nav li').find("div").stop().slideUp()
         });
     })    
@@ -68,21 +87,36 @@ $(function(){
         $('.visual').on('mouseover',function(){
             $('.visual .visualText').addClass('visible')
         });
-
+    
     // faction 컨텐츠 등장 이벤트 입니다.
-    $(window).on('scroll',function(){
-        var sTop = $(this).scrollTop();
-        var winH = $(window).height();
-        var facW = $('.faction_wrap').offset().top
-        if((facW - winH) < sTop){
-            $('.faction_wrap').addClass('show')
-            $('.faction_title').addClass('show')
-        }else{
-            $('.faction_wrap').removeClass('show')
-            $('.faction_title').removeClass('show')
-        }
+    function facAction(){
+        $(window).on('scroll',function(){
+            var sTop = $(this).scrollTop();
+            var winH = $(window).height();
+            var facW = $('.faction_wrap').offset().top
+            if((facW - winH) < sTop){
+                $('.faction_wrap').addClass('show')
+                $('.faction_title').addClass('show')
+            }else{
+                $('.faction_wrap').removeClass('show')
+                $('.faction_title').removeClass('show')
+            }
 
-    });
+        });
+    }
+    // guide 등장 이벤트 입니다.
+    function  guideAction(){
+        $(window).on('scroll',function(){
+            var sTop = $(this).scrollTop();
+            var winH = $(window).height();
+            var guid = $('.guide').offset().top
+            if((guid - winH) < sTop){
+                $('.guide').addClass('show')
+            }else{
+                $('.guide').removeClass('show')
+            } 
+        });
+    }
     // faction 마우스 이벤트 입니다.
         $(function(){
             $('.faction_wrap div').on('mouseover',function(){
@@ -124,38 +158,27 @@ $(function(){
                 t.find('.s_bgf').removeClass('active');
                 // t.find('.s_bgf').slideUp();
             };
-            
-            // footer sns icon change 이벤트 입니다.
-        
-            var snsImg = $('.sns a').find('img').attr('src');
-            setTimeout(function(){
-                $('.sns a').on('mouseenter',function(){
-                    snsImg = $(this).find('img').attr('src');
-                    var snsChange = snsImg.replace('_a','_b');
-                    $(this).find('img').attr('src',snsChange)
-                })
-            },1000);
-            setTimeout(function(){
-                $('.sns a').on('mouseleave',function(){
-                    snsImg = $(this).find('img').attr('src');
-                    var snsChange2 = snsImg.replace('_b','_a');
-                    $(this).find('img').attr('src',snsChange2)
-                })
-            },1000);
         })
-    
-    // guide 등장 이벤트 입니다.
-        $(window).on('scroll',function(){
-            var sTop = $(this).scrollTop();
-            var winH = $(window).height();
-            var guid = $('.guide').offset().top
-            if((guid - winH) < sTop){
-                $('.guide').addClass('show')
-            }else{
-                $('.guide').removeClass('show')
-            } 
-        });
-        
+
+        // footer sns icon change 이벤트 입니다.
+            
+        var snsImg = $('.sns a').find('img').attr('src');
+                
+        $('.sns a').on('mouseenter',function(){
+            snsImg = $(this).find('img').attr('src');
+            var snsChange = snsImg.replace('_a','_b');
+            
+            $(this).find('img').attr('src',snsChange)
+            
+        })
+
+        setTimeout(function(){
+            $('.sns a').on('mouseleave',function(){
+                snsImg = $(this).find('img').attr('src');
+                var snsChange2 = snsImg.replace('_b','_a');
+                $(this).find('img').attr('src',snsChange2)
+            })
+        },1000)
     //slideshow 이벤트 입니다.
         $(function(){
 
@@ -167,31 +190,43 @@ $(function(){
                     //success start
                     var name,mass,cargo,mText,cText,estValue,velocity,estText,vText,hitPoint,spec,wText,iText,sImg;
                     var idx = 0;
-                    $(window).on('scroll',function(){
-                        var sTop = $(this).scrollTop();
-                        var winH = $(window).height();
-                        var shoC = $('.showCase').offset().top
-                        if((shoC - winH) < sTop){
-                            $('.showCase').addClass('show');
-                            $('aside').addClass('show');
-                            showCase(0);
-                        }else{
-                            $('.showCase').removeClass('show');
-                            $('aside').removeClass('show');
-                        } 
-                    });
-                  
+                    showCase(0)
+                    // showCase 등장 이벤트 입니다.
+                    function showAction(){
+                        $(window).on('scroll',function(){
+                            var sTop = $(this).scrollTop();
+                            var winH = $(window).height();
+                            var shoC = $('.showCase').offset().top
+                            if((shoC - winH) < sTop){
+                                $('.showCase').addClass('show');
+                                $('aside').addClass('show');   
+                                showCase(0)
+                            }else{
+                                $('.showCase').removeClass('show');
+                                $('aside').removeClass('show');
+                            } 
+                            
+                        });
+                    }
                     $('.slideShow .slideImg img').on("click",function(){
                         idx = $(this).index();
                         console.log($(index.portfolio)[idx]);
-                        $('.shipInfo').addClass('active');
-                        showCase(idx);
+                        $('.shipInfo .shipTable').addClass('active');
+                        $('.shipInfo .tableImg').addClass('active');
+                        
                         setTimeout(function(){
-                            $('.shipInfo').removeClass('active');
-                        },1000)
+                            showCase(idx);
+                        },500);
+
+                        setTimeout(function(){
+                            $('.shipInfo .shipTable').removeClass('active');                 
+                            $('.shipInfo .tableImg').removeClass('active');
+                        },600);
                     })
                     
                     function showCase(){
+                       
+
                         var $this = index.portfolio[idx];
                         name = $this.Name;
                         mass = $this.Mass;
@@ -209,7 +244,7 @@ $(function(){
                         sImg = $this.sImg; 
 
 
-                        showTable = "<div class='shipDetail'><table>";
+                        showTable = "<div class='shipDetail'><table class='shipTable'>";
                         showTable += "<tbody>";
                         showTable += "<th colspan='2'>"+name+"</th>"
                         showTable += "<tr>";
@@ -238,15 +273,15 @@ $(function(){
                         showTable += "</tr>";
                         showTable += "</tody>";
                         showTable += "</table>";
-                        showTable += "<figure><img src="+sImg+"></figure></div>";
+                        showTable += "<figure><img class='tableImg' src="+sImg+"></figure></div>";
                         $('.shipInfo').html(showTable)
                     }
                     
                 // success end
                 }
             });
-
+            
         });
-        
     // end
 });
+
