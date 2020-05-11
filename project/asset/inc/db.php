fc7f3b69c2f3c9647aaa5b4260ac8297284337d3
lmk206 <59675250+lmk206@users.noreply.github.com>
2020-05-11 18:20:56 +0900

    study
    200511

---
+++ project/asset/inc/db.php
@@ -0,0 +1,18 @@
+<?
+
+session_start();
+
+$host = 'localhost';
+$db_user = 'root';
+$db_password ="";
+$db_name = "lmk206"; //user DB
+
+$dbcon = new mysqli($host, $db_user,
+                   $db_password, $db_name);
+
+function mq($sql){
+    global $dbcon;
+    return $dbcon->query($sql);
+}
+
+?>
